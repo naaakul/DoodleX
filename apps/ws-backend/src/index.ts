1335caaa3ad5dbb748e3b5ab41cd549ae17eadfe
Nakul Chouksey <71.nakul@gmail.com>
2025-02-01 22:40:42 +0200

    first commit

---
+++ apps/ws-backend/src/index.ts
@@ -0,0 +1,9 @@
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 9090 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+});
